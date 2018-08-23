3e8c6abf02a07a6fcc26f1caa0a96e99bb25ce5c
Jef_Wang <wangyizhi@chinacloud.com.cn>
2018-08-23 16:51:25 +0800

    ddd

--- jetose-spring-boot-starter/swagger-spring-boot-starter/src/main/java/com/github/jetose/spring/boot/starter/swagger/configuration/SwaggerAutoConfiguration.java
+++ jetose-spring-boot-starter/swagger-spring-boot-starter/src/main/java/com/github/jetose/spring/boot/starter/swagger/configuration/SwaggerAutoConfiguration.java
@@ -17,6 +17,9 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+/**
+ * 123
+ */
 @Configuration
 @Import({
         SwaggerConfiguration.class
